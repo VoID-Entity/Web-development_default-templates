0e7a96896705fcd42efa602d4217a81018977175
VoID-Entity <the-void-entity@protonmail.com>
2021-06-11 08:56:15 +0200

    test update

--- Default Template - HTML Index and CSS style WITHOUT sidebar/index.php
+++ Default Template - HTML Index and CSS style WITHOUT sidebar/index.php
@@ -1,1 +1,83 @@
+<!DOCTYPE html>
 
+<!--------------------------------------------------------------------------------------------------------------------
+
+Theme Name: Default Template V1
+Author: Patrik Nilsson
+Description: HTML Template
+Version: 1.0
+License: GNU General Public License v2 or later
+Tags: HTML Template, Structure, Layout
+Text Domain: https://github.com/VoID-Entity
+
+-------------------------------------------------------------------------------------------------------------------->
+
+<html>
+	<!--Header-->
+	<head>
+		<!--Character Encoding Table-->
+		<meta charset="UTF-8">
+		<!--Include CSS file-->
+		<link href="style.css" rel="stylesheet">
+		<!--Title-->
+		<title>
+			Default Template V1
+		</title>
+	</head>
+	<!--Body-->
+	<body>
+		<!--Wrapper-->
+		<div class="wrapper">
+			<!--Header-->
+			<header>
+				<div id="header" class="header">
+					<h1>
+						Header
+					</h1>
+				</div>
+			</header>
+			<!--Navigation-->
+			<nav>
+				<div id="nav" class="nav">
+					<li>
+						<a href="index.html">
+							Homepage
+						</a>
+						<a href="#">
+							Nextpage 1
+						</a>
+						<a href="#">
+							Nextpage 2
+						</a>
+						<a href="#">
+							Nextpage 3
+						</a>
+					</li>
+				</div>
+			</nav>
+			<!--Main-->
+			<main>
+				<div id="main" class="main">
+					<div id="content">
+						<section>
+							<h1>
+								Headline / Object
+							</h1>
+							<p>
+								Text / Object
+							</p>
+						</section>
+					</div>
+				</div>
+			</main>
+			<!--Footer-->
+			<footer>
+				<div id="footer" class="footer">
+					<p>
+						&#169;Patrik Nilsson
+					</p>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
